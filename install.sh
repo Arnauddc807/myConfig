ffcf0edd694b9ee1cad5a7da281c1ff7ffdbe27c
Arnaud De Cuyper <arnaud.de.cuyper@telenet.be>
2023-08-14 14:25:11 +0200

    background

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
+#place .config in its place
 cp -r .config ~/.config
-sudo pacman -S xorg-server i3-wm rofi rofi-calc rofi-emoji picom polybar i3blocks i3lock i3status alacritty dmenu pavucontrol fish
+
+#install packages
+sudo pacman -S xorg-server i3-wm rofi picom polybar i3blocks i3lock i3status alacritty dmenu pavucontrol fish xdg-user-dirs
 yay -S ttf-iosevka-nerd noto-fonts-emoji
 
+#install rofi theme
 git clone https://github.com/lr-tech/rofi-themes-collection.git
 rofi-themes-collection
 mkdir -p ~/.local/share/rofi/themes/
 cp themes/squared-nord.rasi ~/.local/share/rofi/themes/
+
+#beautiful background
+mkdir -p $(xdg-user-dir PICTURES)/wallpapers/
+cp win11.jpg $(xdg-user-dir PICTURES)/wallpapers/
+
