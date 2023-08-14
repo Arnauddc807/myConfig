4e8945072213fc675d07040f199c3b00b517d1cc
Arnaud De Cuyper <arnaud.de.cuyper@telenet.be>
2023-08-14 15:20:31 +0200

    added yay

--- install.sh
+++ install.sh
@@ -5,12 +5,21 @@ cp -r .config ~
 
 #install packages
 sudo pacman -S xorg-server xorg-xinit i3-wm rofi picom polybar i3blocks i3lock i3status alacritty dmenu pavucontrol fish xdg-user-dirs
+
+#install yay and its packages
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+rm -fr yay
+
 yay -S ttf-iosevka-nerd noto-fonts-emoji
+
 
 #install rofi theme
 git clone https://github.com/lr-tech/rofi-themes-collection.git
 mkdir -p ~/.local/share/rofi/themes/
 cp rofi-themes-collection/themes/squared-nord.rasi ~/.local/share/rofi/themes/
+rm -fr rofi-themes-collection
 
 #beautiful background
 mkdir -p $(xdg-user-dir PICTURES)/wallpapers/
