11b67905687ad5acfc7289fe2b223181809cacc3
Arnaud De Cuyper <arnaud.de.cuyper@telenet.be>
2023-08-14 13:30:49 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp -r .config ~/.config
+sudo pacman -S xorg-server i3-wm rofi picom polybar i3blocks i3lock i3status alacritty dmenu
+yay -S ttf-iosevka-nerd noto-fonts-emoji
+
+git clone https://github.com/lr-tech/rofi-themes-collection.git
+rofi-themes-collection
+mkdir -p ~/.local/share/rofi/themes/
+cp themes/squared-nord.rasi ~/.local/share/rofi/themes/
