a92e461b39a4f8be33bc06706d1bd0cf032a6da3
Arnaud De Cuyper <arnaud.de.cuyper@telenet.be>
2023-08-25 17:30:38 +0200

    better background location & added missing package

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 cp -r .config ~
 
 #install packages
-sudo pacman -S xorg-server xorg-xinit i3-wm rofi rofi-calc rofi-emoji picom polybar i3blocks i3lock i3status alacritty dmenu pavucontrol fish xdg-user-dirs feh
+sudo pacman -S xorg-server xorg-xinit i3-wm rofi rofi-calc rofi-emoji picom polybar i3blocks i3lock i3status alacritty dmenu pavucontrol fish xdg-user-dirs feh playerctl
 
 #change shell to fish
 chsh -s /bin/fish
@@ -26,6 +26,6 @@ cp rofi-themes-collection/themes/squared-nord.rasi ~/.local/share/rofi/themes/
 rm -fr rofi-themes-collection
 
 #beautiful background
-mkdir -p $(xdg-user-dir PICTURES)/wallpapers/
-cp win11.jpg $(xdg-user-dir PICTURES)/wallpapers/
+mkdir -p /usr/share/backgrounds
+cp win11.jpg /usr/share/backgrounds
 
